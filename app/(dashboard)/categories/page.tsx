0f4384abbde9fdc5df5da380593ac117f25482df
luvvas <luvvasm@gmail.com>
2024-09-04 18:59:13 -0300

    feat: categories ui and api

--- app/(dashboard)/categories/page.tsx
+++ app/(dashboard)/categories/page.tsx
@@ -7,23 +7,23 @@ import { Skeleton } from "@/components/ui/skeleton"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { DataTable } from "@/components/data-table"
 
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account"
-import { useGetAccounts } from "@/features/accounts/api/use-get-accounts"
-import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete-accounts"
+import { useNewCategory } from "@/features/categories/hooks/use-new-category"
+import { useGetCategories } from "@/features/categories/api/use-get-categories"
+import { useBulkDeleteCategories } from "@/features/categories/api/use-bulk-delete-categories"
 
 import { columns } from "./columns"
 
-const AccountsPage = () => {
-    const newAccount = useNewAccount()
-    const accountsQuery = useGetAccounts()
-    const deleteAccounts = useBulkDeleteAccounts()
-    const accounts = accountsQuery.data || []
+const categoriesPage = () => {
+    const newCategory = useNewCategory()
+    const categoriesQuery = useGetCategories()
+    const deleteCategories = useBulkDeleteCategories()
+    const categories = categoriesQuery.data || []
 
     const isDisabled = 
-        accountsQuery.isLoading ||
-        deleteAccounts.isPending
+        categoriesQuery.isLoading ||
+        deleteCategories.isPending
     
-    if(accountsQuery.isLoading) {
+    if(categoriesQuery.isLoading) {
         return(
             <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
                 <Card className="border-none drop-shadow-sm">
@@ -45,9 +45,9 @@ const AccountsPage = () => {
             <Card className="border-none drop-shadow-sm">
                 <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
                     <CardTitle className="text-xl line-clamp-1">
-                        Accounts page
+                        Categories page
                     </CardTitle>
-                    <Button onClick={newAccount.onOpen} size="sm">
+                    <Button onClick={newCategory.onOpen} size="sm">
                         <Plus className="size-4 mr-2" />
                         Add new
                     </Button>
@@ -56,11 +56,11 @@ const AccountsPage = () => {
                     <DataTable 
                         onDelete={(row) => {
                             const ids = row.map((r) => r.original.id)
-                            deleteAccounts.mutate({ ids })
+                            deleteCategories.mutate({ ids })
                         }}
                         filterKey="name"
                         columns={columns} 
-                        data={accounts} 
+                        data={categories} 
                         disabled={isDisabled}
                     />
                 </CardContent>
@@ -69,4 +69,4 @@ const AccountsPage = () => {
     )
 }
 
-export default AccountsPage
+export default categoriesPage
--- features/categories/components/edit-category-sheet.tsx
+++ features/categories/components/edit-category-sheet.tsx
@@ -1,7 +1,6 @@
 import { z } from 'zod'
 
 import { Loader2 } from 'lucide-react'
-import { insertAccountSchema } from '@/db/schema'
 import {
     Sheet,
     SheetContent,
@@ -10,32 +9,34 @@ import {
     SheetTitle
 } from '@/components/ui/sheet'
 
-import { AccountForm } from '@/features/accounts/components/account-form'
-import { useOpenAccount } from '@/features/accounts/hooks/use-open-account'
-import { useConfirm } from '@/hooks/use-confirm'
-import { useGetAccount } from '@/features/accounts/api/use-get-account'
-import { useEditAccount } from '@/features/accounts/api/use-edit-account'
-import { useDeleteAccount } from '@/features/accounts/api/use-delete-account'
+import { CategoryForm } from '@/features/categories/components/category-form'
+import { useGetCategory } from '@/features/categories/api/use-get-category'
+import { useOpenCategory } from '@/features/categories/hooks/use-open-category'
+import { useEditCategory } from '@/features/categories/api/use-edit-category'
+import { useDeleteCategory } from '@/features/categories/api/use-delete-category'
 
-const formSchema = insertAccountSchema.pick({
+import { useConfirm } from '@/hooks/use-confirm'
+import { insertCategorySchema } from '@/db/schema'
+
+const formSchema = insertCategorySchema.pick({
     name: true,
 })
 
 type FormValues = z.input<typeof formSchema>
 
-export const EditAccountSheet = () => {
-    const { isOpen, onClose, id } = useOpenAccount()
+export const EditCategorySheet = () => {
+    const { isOpen, onClose, id } = useOpenCategory()
     const [ConfirmDialog, confirm] = useConfirm(
         "Are you sure?",
-        "You are about to delete this account."
+        "You are about to delete this category"
     )
 
-    const accountQuery = useGetAccount(id)
-    const editMutation = useEditAccount(id)
-    const deleteMutation = useDeleteAccount(id)
+    const categoryQuery = useGetCategory(id)
+    const editMutation = useEditCategory(id)
+    const deleteMutation = useDeleteCategory(id)
 
     const isPending = editMutation.isPending || deleteMutation.isPending
-    const isLoading = accountQuery.isLoading
+    const isLoading = categoryQuery.isLoading
 
     const onSubmit = (values: FormValues) => {
         editMutation.mutate(values, {
@@ -57,8 +58,8 @@ export const EditAccountSheet = () => {
         }
     }
 
-    const defaultValues = accountQuery.data ? {
-        name: accountQuery.data.name
+    const defaultValues = categoryQuery.data ? {
+        name: categoryQuery.data.name
     } : {
         name: ""
     }
@@ -69,8 +70,8 @@ export const EditAccountSheet = () => {
             <Sheet open={isOpen} onOpenChange={onClose}>
                 <SheetContent className='space-y-4'>
                     <SheetHeader>
-                        <SheetTitle>Edit Account</SheetTitle>
-                        <SheetDescription>Edit an existing account.</SheetDescription>
+                        <SheetTitle>Edit Category</SheetTitle>
+                        <SheetDescription>Edit an existing category.</SheetDescription>
                     </SheetHeader>
                     {isLoading
                         ? (
@@ -78,7 +79,7 @@ export const EditAccountSheet = () => {
                                 <Loader2 className='size-4 text-muted-foreground animate-spin' />
                             </div>
                         ) : (
-                            <AccountForm
+                            <CategoryForm
                                 id={id}
                                 onSubmit={onSubmit}
                                 disabled={isPending}
